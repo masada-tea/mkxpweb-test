Ancurio/mkxp

commit 55cec53911f6706f6ad7ae58095644235e2259ba

Sprite: Clamp src_rect to bitmap bounds

src/sprite.cpp
@@ -134,12 +134,23 @@ struct SpritePrivate
 
 	void onSrcRectChange()
 	{
-		if (mirrored)
-			quad.setTexRect(srcRect->toFloatRect().hFlipped());
-		else
-			quad.setTexRect(srcRect->toFloatRect());
+		FloatRect rect = srcRect->toFloatRect();
+		Vec2i bmSize;
 
-		quad.setPosRect(IntRect(0, 0, srcRect->width, srcRect->height));
+		if (bitmap)
+			bmSize = Vec2i(bitmap->width(), bitmap->height());
+
+		if (mirrored)
+			rect = rect.hFlipped();
+
+		/* Clamp the rectangle so it doesn't reach outside
+		 * the bitmap bounds */
+		rect.w = clamp<int>(rect.w, 0, bmSize.x-rect.x);
+		rect.h = clamp<int>(rect.h, 0, bmSize.y-rect.y);
+
+		quad.setTexRect(rect);
+
+		quad.setPosRect(FloatRect(0, 0, rect.w, rect.h));
 		recomputeBushDepth();
 
 		wave.dirty = true;

masada-tea/mkxpweb-test

commit 4dd6788a734f4f2a2e11650c5c3a14900983910b

Sprite: Fix regression with "mirror" attribute
FloatRect::hFlipped() returns a rectangle with negative width,
which was clobbered by the clamping further down.

Regression introduced in 55cec53911f6706f6ad7ae58095644235e2259ba.

src/sprite.cpp
@@ -140,15 +140,12 @@ struct SpritePrivate
 		if (!nullOrDisposed(bitmap))
 			bmSize = Vec2i(bitmap->width(), bitmap->height());
 
-		if (mirrored)
-			rect = rect.hFlipped();
-
 		/* Clamp the rectangle so it doesn't reach outside
 		 * the bitmap bounds */
 		rect.w = clamp<int>(rect.w, 0, bmSize.x-rect.x);
 		rect.h = clamp<int>(rect.h, 0, bmSize.y-rect.y);
 
-		quad.setTexRect(rect);
+		quad.setTexRect(mirrored ? rect.hFlipped() : rect);
 
 		quad.setPosRect(FloatRect(0, 0, rect.w, rect.h));
 		recomputeBushDepth();