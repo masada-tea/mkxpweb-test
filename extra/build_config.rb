3745f75637c2f13bba65c9199656ee9d0b34f8b3
masada-tea <64900337+masada-tea@users.noreply.github.com>
2026-04-27 03:34:24 +0900

    build_config.rb を更新

--- extra/build_config.rb
+++ extra/build_config.rb
@@ -1,11 +1,18 @@
+MRuby::Build.new do |conf|
+    toolchain :gcc
+    conf.gembox 'default'
+end
+
 MRuby::CrossBuild.new('wasm32-unknown-gnu') do |conf|
     toolchain :clang
-  
+
     conf.gembox 'default'
+    # onig-regexp を mruby 2.x 互換コミットで先に宣言して上書き
+    conf.gem :github => 'mattn/mruby-onig-regexp',
+             :checksum_hash => '08decdc7c0fbbf873568a44b1a83b9a72d640d8a'
     conf.gem :github => 'pulsejet/mruby-marshal'
     conf.gem :github => 'monochromegane/mruby-time-strftime'
     conf.gem :core => 'mruby-eval'
-    conf.gem :github => 'mattn/mruby-onig-regexp', :ref => '08decdc'
 
     conf.cc.command = 'emcc'
     conf.cc.flags = %W(-O3 -g0)
