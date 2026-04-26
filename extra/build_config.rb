def81d12b663b141a419691f6f58972da93a149c
masada-tea <64900337+masada-tea@users.noreply.github.com>
2026-04-27 03:30:09 +0900

    build_config.rb を更新

--- extra/build_config.rb
+++ extra/build_config.rb
@@ -1,8 +1,3 @@
-MRuby::Build.new do |conf|
-    toolchain :gcc
-    conf.gembox 'default'
-end
-
 MRuby::CrossBuild.new('wasm32-unknown-gnu') do |conf|
     toolchain :clang
   
@@ -10,12 +5,12 @@ MRuby::CrossBuild.new('wasm32-unknown-gnu') do |conf|
     conf.gem :github => 'pulsejet/mruby-marshal'
     conf.gem :github => 'monochromegane/mruby-time-strftime'
     conf.gem :core => 'mruby-eval'
-    # ↓ これを追加（presym.h 不要な最後のコミットに固定）
+    conf.gem :github => 'mattn/mruby-onig-regexp', :ref => '08decdc'
+
     conf.cc.command = 'emcc'
     conf.cc.flags = %W(-O3 -g0)
     conf.cxx.command = 'em++'
     conf.cxx.flags = %W(-O3 -g0 -std=c++14)
-  
     conf.linker.command = 'emcc'
     conf.archiver.command = 'emar'
 end
