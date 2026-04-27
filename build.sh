99e491f1d051f47787f7b9c9f0a91d0982c06389
masada-tea <64900337+masada-tea@users.noreply.github.com>
2026-04-27 19:10:56 +0900

    build.sh を更新

--- build.sh
+++ build.sh
@@ -64,8 +64,8 @@ then
     git clone https://github.com/emscripten-core/emsdk.git
     cd emsdk
     git pull
-    ./emsdk install 2.0.34
-    ./emsdk activate 2.0.34
+    ./emsdk install 3.1.14
+    ./emsdk activate 3.1.14
     cd ..
 fi
 
@@ -77,7 +77,7 @@ if [ ! -f "libsigc++/sigc++/.libs/libsigc-2.0.a" ]
 then
     cd libsigc++
     emconfigure ./autogen.sh
-    emconfigure ./configure --host=wasm32-unknown-emscripten --enable-static --disable-shared
+    emconfigure ./configure --enable-static --disable-shared
     emmake make clean
     emmake make -j4 || true
     cd ..
