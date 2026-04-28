2d49a97d8c4016c727f6dd9058c62c0c31b44018
masada-tea <masada-tea@users.noreply.github.com>
2026-04-28 14:29:39 +0000

    deploy: 5244637fbb716eeb0313146ce444058b85caf641

---
+++ gameasync/mapping.js
@@ -0,0 +1,32 @@
+var mappingArray = [
+["data", "Data?h="],
+["data/actors", "Data/Actors.rxdata?h=463313932c4373b737ab763c5860915a"],
+["data/animations", "Data/Animations.rxdata?h=edea61711e33a4e284276bf7b0cc5e1b"],
+["data/armors", "Data/Armors.rxdata?h=e395169c716f1fd7a0b2613df0ae6c8f"],
+["data/classes", "Data/Classes.rxdata?h=9ce3a4bd063242efa0934e0da9b546bc"],
+["data/commonevents", "Data/CommonEvents.rxdata?h=33d714659b70225011dc4e75a072a2cb"],
+["data/enemies", "Data/Enemies.rxdata?h=556000ab50ee8918d249b22d8fe62737"],
+["data/items", "Data/Items.rxdata?h=e2b0e901820953a8e3a27b84243fae35"],
+["data/map001", "Data/Map001.rxdata?h=7830c6d19e97e16edfe0b33cdabce7c6"],
+["data/mapinfos", "Data/MapInfos.rxdata?h=458415d2a30dd8d904febdbb4366570a"],
+["data/scripts", "Data/Scripts.rxdata?h=e317907ca7d2ca5a2c08f0e7f232a28f"],
+["data/skills", "Data/Skills.rxdata?h=5ba68f57e98d67fc1cb55ffd3efe4817"],
+["data/states", "Data/States.rxdata?h=8f2b078b98a9c383b3657ad6f89752bc"],
+["data/system", "Data/System.rxdata?h=9dc8b75e924bca1c1307017b7bda7379"],
+["data/tilesets", "Data/Tilesets.rxdata?h=459d7720ee6b34cd7661c19ef0248048"],
+["data/troops", "Data/Troops.rxdata?h=c4648c72337aed54219675cb9c7e8f6c"],
+["data/weapons", "Data/Weapons.rxdata?h=73b98845793f7619081c2c45171ad69c"],
+["game", "Game.exe?h=c22b8d8acb738776d94ea0cc10277144"],
+["game", "Game.ini?h=c84975eba54d16b3243a82588ee3d01f"],
+["game", "Game.rxproj?h=8b3d27798d88da90ec326fcc3e3ccd19"],
+["rgss104j", "RGSS104J.dll?h=ae858cb108e43fa708d81525cd48662e"],
+["bitmap-map", "bitmap-map.js?h=8d29145a907a99d776f9603eb356d5fa"],
+["mapping", "mapping.js?h=9200cbc5596557ab8d881f529a771a0f"],
+["rgss", "rgss.rb?h=e4f6ce1cd6617eedd6e4704cfd0d263b"],
+];
+
+var mapping = {};
+for (var i = 0; i < mappingArray.length; i++) {
+    mapping[mappingArray[i][0]] = mappingArray[i][1];
+}
+
